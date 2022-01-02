81f078eded3b3c7b1b8a51e1fb7ad8918d64dc6d
Arie <nttislinked@gmail.com>
2022-01-02 23:49:07 +0700

    Create is_sum_of_number_a_prime?.cpp

---
+++ luyencode/is_sum_of_number_a_prime?.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(long long n){
+    long long i;
+    int u = 2;
+    if(n < 2) return false;
+    for (i = 2; i <= sqrt(n); i++){
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int sumOfNum(long long n) {
+    int s = 0;
+    while (n != 0) {
+        s += n % 10;
+        n /= 10;
+    }
+    return s;
+}
+
+int main() {
+    long long a, b, i, s;
+    cin >> a >> b;
+    s = 0;
+    for (i = a + 1; i <= b; i++) {
+        if (isPrime(sumOfNum(i))) s++;
+	}
+    cout << s;
+	return 0;
+}
