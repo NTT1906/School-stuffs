7f086b707306d0c0499676e09789fb9d90871013
Arie <nttislinked@gmail.com>
2022-05-09 11:39:24 +0700

    Create snail.cpp

---
+++ luyencode/snail.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    long long a, b, v;
+    cin >> a >> b >> v;
+    if (a == v) {
+        cout<<"1";
+        return 0;
+    } else {
+        if ((V-B)%(A-B)==0) cout<<(V-B)/(A-B);
+        else cout<<(V-B)/(A-B)+1;
+    }
+    return 0;
+}
